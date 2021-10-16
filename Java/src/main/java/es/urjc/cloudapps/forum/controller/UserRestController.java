f7f71fb80e6c4155fd9188e7f7ebc1676e62c0a7
cristoflop <cristofer.lc.99@gmail.com>
2021-10-16 13:13:56 +0200

    Implement users controller

---
+++ Java/src/main/java/es/urjc/cloudapps/forum/controller/UserRestController.java
@@ -0,0 +1,54 @@
+package es.urjc.cloudapps.forum.controller;
+
+import es.urjc.cloudapps.forum.application.UserService;
+import es.urjc.cloudapps.forum.application.dto.NewUserDto;
+import es.urjc.cloudapps.forum.application.dto.UpdateEmailDto;
+import es.urjc.cloudapps.forum.application.dto.UserDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class UserRestController {
+
+    private final UserService userService;
+
+    public UserRestController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<UserDto>> findAllUsers() {
+        List<UserDto> users = userService.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(users);
+    }
+
+    @GetMapping("/users/{userId}")
+    public ResponseEntity<UserDto> findUserById(@PathVariable Long userId) {
+        UserDto user = userService.findById(userId);
+        return ResponseEntity.status(HttpStatus.OK).body(user);
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<UserDto> saveUser(@RequestBody @Valid NewUserDto newUserDto) {
+        UserDto user = userService.save(newUserDto);
+        return ResponseEntity.status(HttpStatus.OK).body(user);
+    }
+
+    @PatchMapping("/users")
+    public ResponseEntity<UserDto> updateUserEmail(@RequestBody @Valid UpdateEmailDto updateEmailDto) {
+        UserDto user = userService.updateEmail(updateEmailDto);
+        return ResponseEntity.status(HttpStatus.OK).body(user);
+    }
+
+    @DeleteMapping("/users/{userId}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long userid) {
+        userService.deleteUser(userid);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+}
