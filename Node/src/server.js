c12627c4d85b1dffb021afd21a4adf91f056d5a3
cristoflop <cristofer.lc.99@gmail.com>
2021-10-21 01:10:04 +0200

    Add main and https server files

---
+++ Node/src/server.js
@@ -0,0 +1,35 @@
+"use strict"
+
+const express = require('express');
+const database = require('./database.js');
+const topicRouter = require('./routes/topicRouter.js');
+const fs = require('fs');
+const https = require('https');
+
+const PORT = require('./config').port;
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/api', topicRouter);
+
+async function main() {
+
+    await database.connect();
+
+    https.createServer({
+        key: fs.readFileSync('src/server.key'),
+        cert: fs.readFileSync('src/server.cert')
+    }, app).listen(PORT, () => {
+        console.log('Forum https server listening on port ' + PORT);
+    })
+
+    process.on('SIGINT', () => {
+        database.disconnect();
+        console.log('Process terminated');
+        process.exit(0);
+    });
+}
+
+main();
